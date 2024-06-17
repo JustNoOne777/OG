8103fe3810d1694e32520f3de7fb2886329a96ef
MIRO <mirianbucxrikidze16@gmail.com>
2024-06-17 22:36:27 +0400

    Add files via upload

--- ext.py
+++ ext.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from os import environ
+
 app= Flask(__name__)
-app.config["SECRET_KEY"]="aofiqnf9qf9qqsfs99983"
+
+
+app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
 db= SQLAlchemy(app)
